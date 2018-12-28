4fa47a72d4d0528a6448c479d632ed3c9d6f4f85
truong <truong.pham@vincere.io>
2018-12-28 09:49:01 +0700

    new scripts

--- staranise/05.job_application.sql
+++
@@ -1,6 +0,0 @@
-select JOB from PROP_X_SHORT_CAND order by JOB desc
-
-select CANDIDATE from PROP_X_SHORT_CAND
---select * from PROP_X_SHORT_CAND -- where reference = 394855
-
-select SHORTLIST from PROP_X_SHORT_CAND
--- staranise/import_document_postgres_expect-script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/expect
-	spawn /bin/sh import_document_postgres.sh
-	expect "Argument list too long"
-	send "/bin/sh import_document_postgres.sh\r"
-	expect "yamla#123"
-	send "bye\r"
