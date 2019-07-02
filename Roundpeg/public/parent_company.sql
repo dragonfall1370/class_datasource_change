dcf303aab4ed302dac7144241943f836e67593cb
BI - Hoa Tran <hoa.tran@vincere.io>
2019-07-02 11:38:32 +0700

    Add projects

--- Roundpeg/public/parent_company.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-a.account_id,
-p.account_id parent_id,
-a.name company_name,
-p.name parent_name
-FROM account a
-LEFT JOIN
-(SELECT DISTINCT account_id, name FROM account) p ON a.parent_id = p.account_id
-WHERE p.name is not NULL
